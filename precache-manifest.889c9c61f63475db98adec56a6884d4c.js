dd12903c4425bb5fafe076db18080036f060276d
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2020-03-13 06:29:51 +0300

    Updates

---
+++ precache-manifest.889c9c61f63475db98adec56a6884d4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8491a4d6a4db574026c5cfefb057b8",
+    "url": "/drego-injunction-scale/index.html"
+  },
+  {
+    "revision": "f123cd0a951ec4bbf96f",
+    "url": "/drego-injunction-scale/static/css/2.54a81f77.chunk.css"
+  },
+  {
+    "revision": "97573c240e3a0cb60707",
+    "url": "/drego-injunction-scale/static/css/main.546a7a5d.chunk.css"
+  },
+  {
+    "revision": "f123cd0a951ec4bbf96f",
+    "url": "/drego-injunction-scale/static/js/2.07f45553.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drego-injunction-scale/static/js/2.07f45553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97573c240e3a0cb60707",
+    "url": "/drego-injunction-scale/static/js/main.63de2a4d.chunk.js"
+  },
+  {
+    "revision": "ac84d45d6aaa6961abfb",
+    "url": "/drego-injunction-scale/static/js/runtime-main.1dce2d39.js"
+  }
+]);
